4e92eaf58c42ea21adab19bf0ea85548cf42a3db
Steve Richert <steve.richert@gmail.com>
2014-07-09 16:37:24 -0400

    Track code coverage

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source "https://rubygems.org"
 gemspec
 
 group :test do
+  gem "codeclimate-test-reporter", require: false
   gem "rspec", "~> 3.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,6 @@
+if ENV["CODECLIMATE_REPO_TOKEN"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require "fair_dice_roll"
